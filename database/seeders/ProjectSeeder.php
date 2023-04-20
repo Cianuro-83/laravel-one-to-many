37a7c65296bf72fa7d86f2b9bc24ba7b8760ed42
Cianuro-83 <buzzeosimone@yahoo.com>
2023-04-20 17:31:58 +0200

    FEAT: POPOLATO DB

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++){
+            $project = new Project();
+            $project->title = $faker->unique()->sentence($faker->numberBetween(5, 7));
+            $project->customer = $faker->name();
+            $project->description = $faker->optional()->text(2500);
+            $project->url = $faker->optional()->url();
+            $project->slug = Str::slug($project->title, '-');
+            $project->save();
+
+        }
     }
 }
